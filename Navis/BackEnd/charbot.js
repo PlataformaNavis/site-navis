902afccac0f84383a80ef593ac28f44c2c5e14cf
Wellington A.S. Aguiar <wellingtonasaguiar@gmail.com>
2025-11-17 18:26:34 -0300

    Add API chatbot OLLAMA

---
+++ Navis/BackEnd/charbot.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { Ollama } from "ollama";
+
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const ollama = new Ollama({
+    host: "https://ollama.com",
+    Authorization: `Bearer ${process.env.OLLAMA_API_KEY}`,
+    }
+);
+
+app.post('/navy', async (req, res) => {
+    const {message} = req.body;
+
+    try {
+            const response = await ollama.chat({
+      model: "gpt-oss:120b-cloud",
+      messages: [{ role: "user", content: message }],
+      stream: true,
+    });
+
+    res.setHeader('Content-Type', 'text/plain')
+    
+    for await (const part of response) {
+      process.stdout.write(part.message.content);
+    }
+
+    res.end();
+    }catch (error) {
+        console.log(`ERROR ${error}`);
+        res.status(500).send('Erro na conexão');
+    }
+});
+
+app.listen(3000, ()=> console.log('Servidor rodando na porta 3000'));
